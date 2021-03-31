ff1f07011ab798f978d1165ff2ba34ef4a9f8294
Mara Kim <mara@autochthe.net>
2021-03-31 01:43:21 -0400

    Rename components

---
+++ KismetBot/Bots/KismetBot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+using KismetParser;
+
+namespace KismetBot.Bots
+{
+    public class KismetBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = $"Echo: {turnContext.Activity.Text}";
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+    }
+}
---
+++ KismetParser/KismetParser.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KismetParser
+{
+    public class KismetParser
+    {
+    }
+}
