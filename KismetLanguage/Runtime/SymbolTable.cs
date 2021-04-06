5dc927cb9ec6f5e7ab2af62558eb1c749a9211e9
Mara Kim <mara@autochthe.net>
2021-04-06 18:10:18 -0400

    Add SymbolTable

---
+++ KismetLanguage/Runtime/SymbolTable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace KismetLanguage.Runtime
+{
+    class UndefinedSymbol { }
+
+    class SymbolTable
+    {
+        private Stack<Dictionary<string, object>> _symbols
+            = new Stack<Dictionary<string, object>>(
+                new[] {
+                    new Dictionary<string, object>(),
+                }
+            );
+
+        public void PushScope()
+        {
+            _symbols.Push(new Dictionary<string, object>());
+        }
+
+        public void PopScope()
+        {
+            _symbols.Pop();
+        }
+
+        public object GetSymbol(string key)
+        {
+            foreach (var frame in _symbols)
+            {
+                if(frame.ContainsKey(key))
+                {
+                    return frame[key];
+                }
+            }
+            return new UndefinedSymbol();
+        }
+
+        public void SetSymbol(string key, object value)
+        {
+            _symbols.Peek()[key] = value;
+        }
+    }
+}
